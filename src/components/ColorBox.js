3977fd4f3979701105ffdacc5edfcd0b6bf2e36d
hyeongmin-park1 <lg3319@gmail.com>
2023-01-11 16:24:57 +0900

    use Context Hook

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ColorContext from '../contexts/color';
 import {ColorConsumer} from '../contexts/color';
 
 const ColorBox = () => {
+    const {state} = useContext(ColorContext);
     return (
         <ColorConsumer>
             {value=>(
@@ -10,13 +11,13 @@ const ColorBox = () => {
                 <div style={{
                     width: '64px',
                     height: '64px',
-                    background: value.state.color
+                    background: state.color
                 }}
                 />
                 <div style={{
                     width: '32px',
                     height: '32px',
-                    background: value.state.subcolor
+                    background: state.subcolor
                 }}
                 />
                 </>
